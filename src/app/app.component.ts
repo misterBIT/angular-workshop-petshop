eaf752e3e017c54ac8f0847186f46d5eed72b15a
nadavsinai <ns@nadavsinai.com>
2016-11-29 00:06:24 +0200

    demo view encapsulization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
   <h1>
+    <span class="logo">
+    <img src="assets/pet/1.png"/>
+    </span>
     {{title}}
   </h1>
   <pet-input></pet-input>
   <pet-list></pet-list>
   `,
-  styles: [`h1 {color:lightblue;}`]
+  styles: [`
+h1 {color:lightblue;}
+.logo {float:left;}
+img {width:50px;} `]
 })
 export class AppComponent {
   title = 'Pet Shop';
