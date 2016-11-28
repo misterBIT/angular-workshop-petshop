e6916df34db63dd1d21eb6679cb3efb602876865
nadavsinai <ns@nadavsinai.com>
2016-11-29 00:00:45 +0200

    add petImg

--- src/app/pet/pet-renderer.component.ts
+++ src/app/pet/pet-renderer.component.ts
@@ -1,18 +1,20 @@
 import {Component, Input} from '@angular/core';
 import {PetModel} from './pet.model';
 @Component({
-  selector:'pet-renderer',
-  template:`
+  selector: 'pet-renderer',
+  styles: [`img {max-height:100px;}`],
+  template: `
           <div class="pet-renderer">
             <span class="petName">{{pet.name}}</span>
             <input type="checkbox" [(ngModel)]="pet.awake">
+            <img [src]="pet.imgUrl"/>
             <p [hidden]="!pet.awake">
                Pet is awake
             </p>
           </div>`
 })
-export class PetRendererComponent{
-  @Input() pet:PetModel;
+export class PetRendererComponent {
+  @Input() pet: PetModel;
 
 
 }
--- src/app/pet/pet.model.ts
+++ src/app/pet/pet.model.ts
@@ -3,8 +3,13 @@ export class PetModel {
   // == Data Members ==
   id: number;
   name: string;
-  awake:boolean;
-  constructor(name = '',awake = true) {
+  awake: boolean;
+
+  get imgUrl(): string {
+    return `assets/pet/${this.id}.png`;
+  }
+
+  constructor(name = '', awake = true) {
     this.name = name;
     this.awake = awake;
     this.id = ++PetModel.maxId;
