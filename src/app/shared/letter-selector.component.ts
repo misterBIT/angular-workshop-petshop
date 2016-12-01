27c0e759470f0a4e8aec7ea6a41c2fe12a155df9
nadavsinai <ns@nadavsinai.com>
2016-12-01 09:30:42 +0200

    add show all button

--- src/app/shared/letter-selector.component.ts
+++ src/app/shared/letter-selector.component.ts
@@ -5,6 +5,7 @@ import {Component, Output, EventEmitter, Input} from '@angular/core';
     styles: [`ul {list-style:none}`, `li {display:inline-block; padding:0 5px;}`, `.selected{background-color:lightblue}`],
     template: `
     <ul>
+        <li><button  [class.selected]="letterSelected === ''" (click)="selectLetter('')">Show All</button></li>
         <li *ngFor="let letter of letters">
             <button [class.selected]="letter.toLowerCase() === letterSelected.toLowerCase()" (click)="selectLetter(letter)">{{letter}}</button>
         </li>
