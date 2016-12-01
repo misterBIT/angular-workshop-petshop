6f9f3a7dd039c5213e0913a0b1fee53f0eea4df8
nadavsinai <ns@nadavsinai.com>
2016-12-01 09:23:03 +0200

    hook up letter selector

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {LetterSelectorComponent} from './letter-selector.component';
 
 @NgModule({
   providers: [],
-  declarations: [],
-  imports: [],
-  exports: [CommonModule, FormsModule],
+  declarations: [LetterSelectorComponent],
+  imports: [CommonModule],
+  exports: [CommonModule, FormsModule, LetterSelectorComponent],
 })
 export class SharedModule {
 }
