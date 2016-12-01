5207b5d3fc5f1986e02004b3af4722310fe5df9a
nadavsinai <ns@nadavsinai.com>
2016-12-01 10:51:01 +0200

    exercise 2 - add countdown component

--- src/app/time/time.module.ts
+++ src/app/time/time.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core'
 import {TimeComponent} from './time.component';
 import {SharedModule} from '../shared/shared.module';
+import {CountdownComponent} from './countdown.component';
 @NgModule({
   providers: [],
-  declarations: [TimeComponent],
+  declarations: [TimeComponent,CountdownComponent],
   imports: [SharedModule],
-  exports: [TimeComponent],
+  exports: [TimeComponent,CountdownComponent],
 })
 export class TimeModule {
 }
