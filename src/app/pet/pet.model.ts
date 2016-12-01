cc437d261266aa7816202e37615d0e32f60ea22c
nadavsinai <ns@nadavsinai.com>
2016-12-01 09:46:09 +0200

    exercise 2 - change model to allow for nextFeed behaviour

--- src/app/pet/pet.model.ts
+++ src/app/pet/pet.model.ts
@@ -1,9 +1,11 @@
 export class PetModel {
   static maxId: number = 0;
+  private static feedingLastForMillis = 20000;
   // == Data Members ==
   id: number;
   name: string;
   awake: boolean;
+  lastFed:number;
 
   get imgUrl(): string {
     return `assets/pet/${this.id}.png`;
@@ -13,10 +15,20 @@ export class PetModel {
     this.awake = !this.awake;
   }
 
+  feed() {
+    this.lastFed = Date.now();
+    this.awake = true;
+  }
+
+  get nextFeedAt() {
+    return this.lastFed + PetModel.feedingLastForMillis;
+  }
+
   constructor(name = '', awake = true) {
     this.name = name;
     this.awake = awake;
     this.id = ++PetModel.maxId;
+    this.lastFed = Date.now();
   }
 
 
